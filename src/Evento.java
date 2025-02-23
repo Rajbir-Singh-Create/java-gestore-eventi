f1e0cc7be5e191a684323582505ea068a0c15414
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-02-23 15:09:51 +0100

    feat: step 1 and step 2 basic function implemented

--- src/Evento.java
+++ src/Evento.java
@@ -1,3 +1,109 @@
+
+import java.time.LocalDate;
+
 public class Evento {
+    // Proprietà
+    private String titolo;
+    private LocalDate data;
+    private int postiTotali;
+    private int postiPrenotati;
+    private int postiDisponibili;
     
+    // Quando si istanzia un nuovo evento questi attributi devono essere tutti valorizzati nel costruttore,
+    // tranne posti prenotati che va inizializzato a 0.
+    public Evento(String titolo, LocalDate data, int postiTotali) {
+        this.titolo = titolo;
+        checkDate(data);
+        checkPostiTotali(postiTotali);
+        this.postiPrenotati = 0;
+        setPostiDisponibili();
+    }
+
+    //* getters e setters
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        checkDate(data);
+    }
+
+    public int getPostiTotali() {
+        return postiTotali;
+    }
+
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    public void setPostiDisponibili() {
+        postiDisponibili = getPostiTotali() - getPostiPrenotati();
+    }
+
+    public int getPostiDisponibili(){
+        // postiDisponibili = getPostiTotali() - getPostiPrenotati();
+        return postiDisponibili;
+    }
+
+    //* Metodi di servizio
+    // TODO: gestire le eccezioni
+    public int prenota(){
+        checkDate(data);
+        if (getPostiDisponibili() > getPostiPrenotati()){
+            postiPrenotati += 1;
+            setPostiDisponibili();
+            return postiPrenotati;
+        } else {
+            throw new IllegalArgumentException("Non ci sono più posti disponibili.");
+        }
+    }
+
+    public int disdici(){
+        checkDate(data);
+        if (getPostiPrenotati() > 0){
+            postiPrenotati -= 1;
+            setPostiDisponibili();
+            return postiPrenotati;
+        } else {
+            throw new IllegalArgumentException("Non ci sono posti prenotati.");
+        }
+    }
+
+    // private int calcolaPostiDisponibili(){
+    //     postiDisponibili = getPostiTotali() - getPostiPrenotati();
+    //     return postiDisponibili;
+    // }
+
+    //* metodi di servizio per eseguire controlli
+    private LocalDate checkDate(LocalDate data){
+        if(data.isAfter(data.now())){
+            return this.data = data;
+        } else {
+            throw new IllegalArgumentException("Non puoi inserire una data nel passato.");
+        }
+    }
+
+    private int checkPostiTotali(int postiTotali){
+        if(postiTotali > 0){
+            return this.postiTotali = postiTotali;
+        } else {
+            throw new IllegalArgumentException("Deve esserci almeno un posto.");
+        }
+    }
+
+    //* override metodo toString()
+    // TODO: formattare correttamente la data
+    @Override
+    public String toString() {
+        return getData() + " - " + getTitolo();
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,92 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Step 2-1
+        // Chiedo all'utente di inserire un nuovo evento con tutti i parametri
+        // TODO: inserire un controllo per il titolo
+        System.out.println("Benvenuto, inserisci un nuovo evento:");
+        System.out.println("Titolo:");
+        Scanner scanTitolo = new Scanner(System.in);
+        String titolo = scanTitolo.nextLine();
+
+        System.out.println("Data evento: (dd/MM/yyy)");
+        Scanner scanData = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataInput = scanData.nextLine();
+        LocalDate data = LocalDate.parse(dataInput, formatter);
+
+        System.out.println("Posti totali evento:");
+        Scanner scanPostiTotali = new Scanner(System.in);
+        int postiTotali = scanPostiTotali.nextInt();
+
+        // creazione evento - istanziazione
+        Evento evento1 = new Evento(titolo, data, postiTotali);
+
+        // Step 2-2
+        // chiedere all’utente se e quante prenotazioni vuole fare e provare ad effettuarle, implementando opportuni controlli
+        System.out.println("Vuoi fare delle prenotazioni? Y/n");
+        Scanner scanInserimento = new Scanner(System.in);
+        String sceltaInserimento = scanInserimento.nextLine();
+        
+        if(sceltaInserimento.equalsIgnoreCase("y")){
+            // farne un metodo?
+            System.out.println("Inserisci il numero di posti che vuoi prenotare:");
+            Scanner scanNumPrenotazioni = new Scanner(System.in);
+            int numPrenotazioni = scanNumPrenotazioni.nextInt();
+
+            // ciclo for che richiama il metodo per il numero di volte inserito dall'utente
+            for(int i = 0; i < numPrenotazioni; i++){
+                evento1.prenota();
+            }
+
+            // Step 2-3
+            // TODO: farne un metodo
+            // Stampare a video il numero di posti prenotati e quelli disponibili
+            System.out.println("Posti prenotati: " + evento1.getPostiPrenotati());
+            System.out.println("Posti disponibili: " + evento1.getPostiDisponibili());
+            System.out.println(evento1.toString());
+        } else {
+            // Step 2-3
+            // Stampare a video il numero di posti prenotati e quelli disponibili
+            System.out.println("Posti prenotati: " + evento1.getPostiPrenotati());
+            System.out.println("Posti disponibili: " + evento1.getPostiDisponibili());
+            System.out.println(evento1.toString());
+            return;
+        }
+
+        // Step 2-4, 2-5
+        // Chiedere all’utente se e quanti posti vuole disdire
+        System.out.println("Vuoi disdire delle prenotazioni? Y/n");
+        Scanner scanCancellazione = new Scanner(System.in);
+        String sceltaCancellazione = scanCancellazione.nextLine();
+
+        if(sceltaCancellazione.equalsIgnoreCase("y")){
+            // farne un metodo?
+            System.out.println("Inserisci il numero di posti che vuoi disdire:");
+            Scanner scanNumCancellazioni = new Scanner(System.in);
+            int numCancellazioni = scanNumCancellazioni.nextInt();
+
+            // ciclo for che richiama il metodo per il numero di volte inserito dall'utente
+            for(int i = 0; i < numCancellazioni; i++){
+                evento1.disdici();
+            }
+
+            // Step 2-6
+            // Stampare a video il numero di posti prenotati e quelli disponibili
+            System.out.println("Posti prenotati: " + evento1.getPostiPrenotati());
+            System.out.println("Posti disponibili: " + evento1.getPostiDisponibili());
+            System.out.println(evento1.toString());
+        } else {
+            // Step 2-3
+            // Stampare a video il numero di posti prenotati e quelli disponibili
+            System.out.println("Posti prenotati: " + evento1.getPostiPrenotati());
+            System.out.println("Posti disponibili: " + evento1.getPostiDisponibili());
+            System.out.println(evento1.toString());
+            return;
+        }
     }
 }
